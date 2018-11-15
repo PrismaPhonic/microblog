1a533627a7eed551d7bddf1fe701e05753c85332
Peter Farr <Peter@PrismaPhonic.com>
2018-11-14 17:19:21 -0800

    Setup a comment system and drilled that baby deep

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { Container } from 'reactstrap';
 import Routes from './Routes';
 import Header from './Header';
 import './App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faEdit, faTrashAlt)
 
 class App extends Component {
   render() {
